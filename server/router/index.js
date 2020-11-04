11954f6b33af167f56401656a350ce8bc7a421db
DoneDidley <38599829+DoneDidley@users.noreply.github.com>
2020-11-04 14:41:32 +1100

    removed user route for, replaced with auth route

--- server/router/index.js
+++ server/router/index.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 
 router.use("/diary", require("./diary"));
 router.use("/foods", require("./foods"));
-router.use("/user", require("./user"));
 router.use("/auth", require("./auth"));
 
 module.exports = router;
