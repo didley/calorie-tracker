6fd668fc91140afb0fef29bae52af117c6856353
DoneDidley <38599829+DoneDidley@users.noreply.github.com>
2020-12-22 14:28:47 +1100

    Deleted unused directories exapmlePhotos & mockServer and progress on config for es6 modules

--- api/src/db/index.js
+++ api/src/db/index.js
@@ -1,18 +1,11 @@
-const { connect } = require("mongoose");
+import mongoose from "mongoose";
 
-module.exports = {
-  async createDBConnection() {
-    try {
-      await connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        autoIndex: false,
-      });
-      console.log("🚀   Connected to DB successfully!");
-    } catch (err) {
-      console.log(err.message);
-      process.exit(1); // Exit process on failure
-    }
-  },
+export const connect = () => {
+  mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    autoIndex: false,
+  });
+  console.log("🚀   Connected to DB successfully!");
 };
--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
-require("dotenv").config();
-const express = require("express");
-const cookieParser = require("cookie-parser");
+// const express = require("express");
+import express from "express";
+// const cookieParser = require("cookie-parser");
+import cookieParser from "cookie-parser";
 const bodyParser = require("body-parser");
 
 const session = require("express-session");
@@ -9,34 +10,34 @@ const passport = require("passport");
 
 const { createDBConnection } = require("./db");
 
-const initializeExpress = () => {
-  const app = express();
-  app.use(cookieParser());
-  app.use(bodyParser.json());
+const app = express();
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-  app.use(
-    session({
-      secret: process.env.APP_SECRET,
-      store: new MongoStore({ url: process.env.DB_URI }),
-      resave: false,
-      saveUninitialized: false,
-    })
-  );
+app.use(
+  session({
+    secret: process.env.APP_SECRET,
+    store: new MongoStore({ url: process.env.DB_URI }),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-  app.use(passport.initialize());
-  app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-  app.use("/api", require("./router"));
-
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`🚀   Server running on PORT ${PORT}`);
-  });
-};
+app.use("/api", require("./router"));
 
 const initializeApp = async () => {
-  await createDBConnection();
-  initializeExpress();
+  try {
+    await createDBConnection();
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`🚀   Server running on PORT ${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 initializeApp();
