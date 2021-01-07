3619cde058b5bb38c40a4e398c9cf608d9d96245
DoneDidley <38599829+DoneDidley@users.noreply.github.com>
2021-01-07 15:47:30 +1100

    Progress on account page, implemented Formik from library

--- client_web/src/components/Account/index.js
+++ client_web/src/components/Account/index.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { Page, Container, Button } from "components/shared/styling";
+import { Formik, Form, Field } from "formik";
+import { CountryDropdown } from "react-country-region-selector";
 
 import { useAuth } from "hooks/useAuth";
 
@@ -21,100 +23,108 @@ import { useAuth } from "hooks/useAuth";
 // Delete Account
 
 export default function Account() {
-  const [formData, setFormData] = useState({});
-  const auth = useAuth();
+  const { user, updateUser } = useAuth();
 
   // TODO: working on
   return (
     <Page>
       <Container>
-        <button onClick={async () => await auth.updateUser({ name: "jack" })}>
-          Test to update user
-        </button>
-        <form>
-          <h1>Account</h1>
-          <fieldset>
-            <legend>
-              <h6>Your details</h6>
-            </legend>
-            <div className="flex">
-              <label htmlFor="name">
-                Name
-                <input />
-              </label>
-              <label htmlFor="date of birth">
-                Date of birth
-                <input />
-              </label>
-              <label htmlFor="sex">
-                Sex
-                <input />
-              </label>
-            </div>
-            <div className="flex">
-              <label htmlFor="height">
-                Height
-                <input />
-              </label>
-              <label htmlFor="current weight">
-                Current weight
-                <input />
-              </label>
-            </div>
+        <h1>Account</h1>
+        <Formik initialValues={user} onSubmit={updateUser}>
+          <Form>
+            <fieldset>
+              <legend>
+                <h6>Your details</h6>
+              </legend>
+              <div className="flex">
+                <label htmlFor="name">
+                  Name
+                  <Field name="name" />
+                </label>
+                <label htmlFor="date of birth">
+                  Date of birth
+                  <Field name="dateOfBirth" />
+                </label>
+                <label htmlFor="sex">
+                  Sex
+                  <Field name="sex" component="select">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </Field>
+                </label>
+              </div>
+              <div className="flex">
+                <label htmlFor="height">
+                  Height
+                  <Field name="heightCm" />
+                </label>
+                <label htmlFor="current weight">
+                  Current weight
+                  <Field name="currentWeightKg" />
+                </label>
+              </div>
+            </fieldset>
             <br />
-          </fieldset>
-          <fieldset>
-            <legend>
-              <h6>Goals</h6>
-            </legend>
-            <label htmlFor="weight goal">
-              Weight goal
-              <input />
-            </label>
-            <label htmlFor="energy goal">
-              Energy goal
-              <input />
-            </label>
+            <fieldset className="flex">
+              <legend>
+                <h6>Goals</h6>
+              </legend>
+              <label htmlFor="weight goal">
+                Weight goal
+                <Field name="goals.weightGoalKg" />
+              </label>
+              <label htmlFor="energy goal">
+                Energy goal
+                <Field name="goals.energyGoalKJ" />
+              </label>
+            </fieldset>
             <br />
-          </fieldset>
-          <fieldset>
-            <legend>
-              <h6>Preferences</h6>
-            </legend>
-            <label htmlFor="country">
-              Country
-              <input />
-            </label>
-            <label htmlFor="metric system">
-              Metric system
-              <input />
-            </label>
-            <label htmlFor="KJ or Cal">
-              KJ or Cal
-              <input />
-            </label>
+            <fieldset className="flex">
+              <legend>
+                <h6>Preferences</h6>
+              </legend>
+              <label htmlFor="country">
+                Country
+                <CountryDropdown name="country" />
+              </label>
+              <label htmlFor="units">
+                Units
+                <Field name="preferences.metricSystem" component="select">
+                  <option value="true">kg, cm</option>
+                  <option value="false">lbs, in</option>
+                </Field>
+              </label>
+              <label htmlFor="energy ">
+                Energy
+                <Field name="preferences.useKJ" component="select">
+                  <option value="true">Kilojoules</option>
+                  <option value="false">Calories</option>
+                </Field>
+              </label>
+            </fieldset>
             <br />
-          </fieldset>
-          <fieldset>
-            <legend>
-              <h6>Account Details</h6>
-            </legend>
-            <label htmlFor="email">
-              Email
-              <input />
-            </label>
-            <label htmlFor="password">
-              Password
-              <input />
-            </label>
-          </fieldset>
-          <fieldset className="flex justify-between">
-            <button>
-              <small className="text-red-600">Delete Account</small>
-            </button>
-            <Button className="justify-right">Save</Button>
-          </fieldset>
-        </form>
+            <fieldset className="flex">
+              <legend>
+                <h6>Account Details</h6>
+              </legend>
+              <label htmlFor="email">
+                Email
+                <Field name="email" />
+              </label>
+              <label htmlFor="password">
+                Password
+                <Field name="password" />
+              </label>
+            </fieldset>
+            <br />
+            <fieldset className="flex justify-between">
+              <button>
+                <small className="text-red-600">Delete Account</small>
+              </button>
+              <Button className="justify-right">Save</Button>
+            </fieldset>
+          </Form>
+        </Formik>
       </Container>
     </Page>
   );
