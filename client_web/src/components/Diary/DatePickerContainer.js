df963620c314c33cadb3226770084c84275abf9a
DoneDidley <38599829+DoneDidley@users.noreply.github.com>
2021-02-20 18:23:35 +1100

    DatePicker styling changes

--- client_web/src/components/Diary/DatePickerContainer.js
+++ client_web/src/components/Diary/DatePickerContainer.js
@@ -15,31 +15,35 @@ export default function DatePickerContainer({
   }
 
   return (
-    <div className="flex items-center justify-center w-full max-w-sm bg-white p-3 m-2 rounded-lg shadow-lg">
-      <label className="mx-2">Date</label>
-      <button
-        onClick={() => handleDateChange(adjustDate(selectedDate, -1))}
-        className="text-center text-sm appearance-none bg-gray-300 border text-gray-700 py-1 px-3 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:text-red-500 hover:bg-gray-200"
-      >
-        {"<"}
-      </button>
-      <DatePicker
-        dateFormat="dd/MM/yyyy"
-        selected={Date.parse(selectedDate)}
-        onChange={(date) => handleDateChange(date)}
-        className="w-24 mx-1 text-center text-sm appearance-none border text-gray-700 px-2 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:text-red-500 hover:bg-gray-100"
-      />
+    <div className="inline-grid grid-cols-5 items-center w-full max-w-sm bg-white p-3 m-2 rounded-lg shadow-lg">
+      <label className="col-span-1 justify-self-center">Date</label>
 
-      <button
-        onClick={() => handleDateChange(adjustDate(selectedDate, +1))}
-        className="text-center text-sm appearance-none bg-gray-300 border text-gray-700 py-1 px-3 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:text-red-500 hover:bg-gray-200"
-      >
-        {">"}
-      </button>
+      <div className="col-span-3 inline-grid grid-cols-5 gap-2">
+        <button
+          onClick={() => handleDateChange(adjustDate(selectedDate, -1))}
+          className="col-span-1 text-center text-sm appearance-none bg-gray-300 border text-gray-700 px-3 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:text-red-500 hover:bg-gray-200"
+        >
+          {"<"}
+        </button>
+        <div className="col-span-3">
+          <DatePicker
+            dateFormat="dd/MM/yyyy"
+            selected={Date.parse(selectedDate)}
+            onChange={(date) => handleDateChange(date)}
+            className="w-full text-center text-sm appearance-none border text-gray-700 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:text-red-500 hover:bg-gray-100"
+          />
+        </div>
+        <button
+          onClick={() => handleDateChange(adjustDate(selectedDate, +1))}
+          className="col-span-1 text-center text-sm appearance-none bg-gray-300 border text-gray-700 px-3 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:text-red-500 hover:bg-gray-200"
+        >
+          {">"}
+        </button>
+      </div>
       {today !== selectedDate && (
         <button
           onClick={() => handleDateChange(today)}
-          className="text-center text-xs appearance-none text-gray-500 py-1 px-2 mx-1 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:text-red-500 hover:bg-gray-200"
+          className="justify-self-center col-span-1 text-center text-xs appearance-none text-gray-500 py-1 px-2 mx-1 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 hover:text-red-500 hover:bg-gray-200"
         >
           Today
         </button>
