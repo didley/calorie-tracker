4b984e48876eb760d75127b8650545b35c5a9e8e
DoneDidley <38599829+DoneDidley@users.noreply.github.com>
2021-03-17 21:54:57 +1100

    Fixes NaN displaying when no user goal is set

--- client_web/src/components/Diary/SummaryMenu.js
+++ client_web/src/components/Diary/SummaryMenu.js
@@ -5,6 +5,7 @@ import { toCal } from "utils/foodEnegy";
 export default function SummaryMenu({ totalEatenKJ, viewAsCal }) {
   const { user } = useAuth();
   let goal = user.goals.energyGoalKJ;
+
   if (viewAsCal) {
     goal = toCal(goal);
     totalEatenKJ = toCal(totalEatenKJ);
@@ -20,23 +21,26 @@ export default function SummaryMenu({ totalEatenKJ, viewAsCal }) {
     <div className="inline-grid grid-cols-4 divide-x text-center items-center w-64">
       <div>
         <h6 className={remaining.isOver ? "text-red-500" : undefined}>
-          {remaining.percent}%
+          {!goal ? "-" : `${remaining.percent}%`}
         </h6>
       </div>
+
       <div>
         <small className="text-gray-600">Goal</small>
-        <p className="m-0">{goal}</p>
+        <p className="m-0">{!goal ? "Not set" : goal}</p>
       </div>
+
       <div>
         <small className="text-gray-600">Eaten</small>
         <p className={remaining.isOver ? "text-red-500" : undefined}>
           {totalEatenKJ}
         </p>
       </div>
+
       <div>
         <small className="text-gray-600">Left</small>
         <p className={remaining.isOver ? "text-red-500" : undefined}>
-          {remaining.value}
+          {!goal ? "-" : remaining.value}
         </p>
       </div>
     </div>
