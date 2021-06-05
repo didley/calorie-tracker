68bf47c67ae3dd73dc3b6cd68ed4dfec5e94f971
DoneDidley <38599829+DoneDidley@users.noreply.github.com>
2021-06-05 19:40:58 +1000

    added return if already bool

--- client_web/src/utils/parseBoolString.js
+++ client_web/src/utils/parseBoolString.js
@@ -1,16 +1,21 @@
 /**
  * Parses 'true' or 'false' strings to boolean values
  * @param {string} boolString - pass 'true' or 'false' strings
- * @param {boolean=} fallbackBool - Optional fallback boolean, if not supplied will return nothing
+ * @param {boolean=} fallbackBool - Optional fallback boolean, if not supplied will return undefined
  * @returns {boolean}
  */
 export const parseBoolString = (boolString, fallbackBool) => {
+  if (boolString === true || false) return boolString;
+
   if (boolString === "true") {
     return true;
   } else if (boolString === "false") {
     return false;
   } else {
-    console.error("parseBoolString error, string not 'true' or 'false'");
+    console.error(
+      `parseBoolString error, string not 'true' or 'false',
+      boolString: ${boolString}`
+    );
     if (fallbackBool === true) {
       return true;
     } else if (fallbackBool === false) {
